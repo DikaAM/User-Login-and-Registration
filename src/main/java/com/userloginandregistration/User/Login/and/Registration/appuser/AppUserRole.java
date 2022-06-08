ec451250c9be2944d8846c418f5285287b8afe64
DikaAM <fandika906@gmail.com>
2022-06-08 10:57:52 +0700

    User Login and Registration with Email Verifiation (Backend)

---
+++ src/main/java/com/userloginandregistration/User/Login/and/Registration/appuser/AppUserRole.java
@@ -0,0 +1,6 @@
+package com.userloginandregistration.User.Login.and.Registration.appuser;
+
+public enum AppUserRole {
+    ROLE_USER,
+    ROLE_ADMIN
+}
---
+++ src/main/java/com/userloginandregistration/User/Login/and/Registration/email/EmailSender.java
@@ -0,0 +1,5 @@
+package com.userloginandregistration.User.Login.and.Registration.email;
+
+public interface EmailSender {
+    void send(String to, String email);
+}
